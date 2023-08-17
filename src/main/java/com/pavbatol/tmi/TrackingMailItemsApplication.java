9e35630bf23baccbc99afac4757a2f52d587cd05
P.Sergey <pavbatol@yandex.ru>
2023-08-17 13:18:34 +0400

    initial commit

---
+++ src/main/java/com/pavbatol/tmi/TrackingMailItemsApplication.java
@@ -0,0 +1,13 @@
+package com.pavbatol.tmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingMailItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingMailItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavbatol/tmi/TrackingMailItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavbatol.tmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingMailItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
