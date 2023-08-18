622d6240f6cf9683281ae87f759f7e3346cc89d0
P.Sergey <pavbatol@yandex.ru>
2023-08-18 18:49:02 +0400

    feat: Add Checker

---
+++ src/main/java/com/pavbatol/tmi/app/util/Checker.java
@@ -0,0 +1,33 @@
+package com.pavbatol.tmi.app.util;
+
+import com.pavbatol.tmi.app.exception.NotFoundException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ResolvableType;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Checker {
+    public static final String S_WITH_ID_S_WAS_NOT_FOUND = "%s with id=%s was not found";
+
+    @NotNull
+    public static <T, I> T getNonNullObject(@NotNull CrudRepository<T, I> storage, I id) throws NotFoundException {
+        return storage.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format(S_WITH_ID_S_WAS_NOT_FOUND, getTClass(storage).getSimpleName(), id)));
+    }
+
+    public static <T, I> void checkId(@NotNull CrudRepository<T, I> storage, I id) throws NotFoundException {
+        if (!storage.existsById(id)) {
+            throw new NotFoundException(String.format(S_WITH_ID_S_WAS_NOT_FOUND, getTClass(storage).getSimpleName(), id));
+        }
+    }
+
+    private static <T, I> Class<T> getTClass(CrudRepository<T, I> storage) {
+        ResolvableType resolvableType = ResolvableType.forClass(storage.getClass()).as(CrudRepository.class);
+        return (Class<T>) resolvableType.getGeneric(0).toClass();
+    }
+}
